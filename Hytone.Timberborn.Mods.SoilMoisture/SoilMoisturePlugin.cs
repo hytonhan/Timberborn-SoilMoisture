86149441044d636832080b300b1b48eace5b8239
Hytone <hannu_94@hotmail.com>
2022-10-05 22:16:56 +0300

    Add project files.

---
+++ Hytone.Timberborn.Mods.SoilMoisture/SoilMoisturePlugin.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using TimberApi.ConfigSystem;
+using TimberApi.ConsoleSystem;
+using TimberApi.ModSystem;
+using Timberborn.SoilMoistureSystem;
+
+namespace Hytone.Timberborn.Mods.Random
+{
+    [HarmonyPatch]
+    public class SoilMoisturePlugin : IModEntrypoint
+    {
+        private static SoilMoistureConfig _config;
+
+        public void Entry(IMod mod, IConsoleWriter consoleWriter)
+        {
+            _config = mod.Configs.Get<SoilMoistureConfig>();
+
+            var harmony = new Harmony("hytone.plugins.random");
+            harmony.PatchAll();
+
+        }
+
+        [HarmonyPatch(typeof(SoilMoistureSimulator), nameof(SoilMoistureSimulator.Load))]
+        public static void Postfix(SoilMoistureSimulator __instance)
+        {
+            __instance._soilMoistureSimulationSettings._maxMoisture = _config.MoistureDistance;
+        }
+    }
+
+    public class SoilMoistureConfig : IConfig
+    {
+        public string ConfigFileName => "SoilMoisture";
+
+        public int MoistureDistance = 16;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# SoilMoistureChanger
+
+Readme coming later.
+
+# Usage
+Change the value of "MoistureDistance" in the config, which is probably in BepInEx\plugins\SoilMoistureChanger\configs\SoilMoisture.json
+
+If the config is not showing, try to launch the game and start up a save, that should create it.
+
+# Change log
+
+## v0.0.1 - 5.10.2022
+- Initial release
