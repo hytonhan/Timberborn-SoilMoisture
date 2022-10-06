82a17e6cf8f5c2d13d90216f64a94878fc6350e5
Hytone <hannu_94@hotmail.com>
2022-10-06 20:17:38 +0300

    Added UphillPenalty to config

---
+++ Hytone.Timberborn.Mods.SoilMoisture/SoilMoistureConfig.cs
@@ -0,0 +1,15 @@
+﻿using TimberApi.ConfigSystem;
+
+namespace Hytone.Timberborn.Mods.SoilMoisture
+{
+    /// <summary>
+    /// Represent the JSON config that is used by the mod
+    /// </summary>
+    public class SoilMoistureConfig : IConfig
+    {
+        public string ConfigFileName => "SoilMoisture";
+
+        public int MoistureDistance = 16;
+        public int UphillPenalty = 6;
+    }
+}
