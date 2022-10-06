f4f24a30fd967c991907abb09a74f2e392491407
Hytone <hannu_94@hotmail.com>
2022-10-06 20:18:12 +0300

    Moved config to itäs own file. Added uphillpenalty in patch

--- Hytone.Timberborn.Mods.SoilMoisture/SoilMoisturePlugin.cs
+++ Hytone.Timberborn.Mods.SoilMoisture/SoilMoisturePlugin.cs
@@ -1,12 +1,13 @@
 ﻿using HarmonyLib;
-using System;
-using TimberApi.ConfigSystem;
 using TimberApi.ConsoleSystem;
 using TimberApi.ModSystem;
 using Timberborn.SoilMoistureSystem;
 
-namespace Hytone.Timberborn.Mods.Random
+namespace Hytone.Timberborn.Mods.SoilMoisture
 {
+    /// <summary>
+    /// Entrypoint of the mod
+    /// </summary>
     [HarmonyPatch]
     public class SoilMoisturePlugin : IModEntrypoint
     {
@@ -16,23 +17,21 @@ namespace Hytone.Timberborn.Mods.Random
         {
             _config = mod.Configs.Get<SoilMoistureConfig>();
 
-            var harmony = new Harmony("hytone.plugins.random");
+            var harmony = new Harmony("hytone.plugins.soilmoisture");
             harmony.PatchAll();
 
         }
 
+        /// <summary>
+        /// Patch the SoilMoistureSimulator.Load to alter the SoilMoistureSimulator values
+        /// </summary>
+        /// <param name="__instance"></param>
         [HarmonyPatch(typeof(SoilMoistureSimulator), nameof(SoilMoistureSimulator.Load))]
         public static void Postfix(SoilMoistureSimulator __instance)
         {
             __instance._soilMoistureSimulationSettings._maxMoisture = _config.MoistureDistance;
+            __instance._soilMoistureSimulationSettings._verticalSpreadCostMultiplier = _config.UphillPenalty;
         }
-    }
-
-    public class SoilMoistureConfig : IConfig
-    {
-        public string ConfigFileName => "SoilMoisture";
-
-        public int MoistureDistance = 16;
     }
 
 }
