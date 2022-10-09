9fe44bed3f8718c571a45f43cd9077bc98223055
the-tanstaafl <105609556+the-tanstaafl@users.noreply.github.com>
2022-10-09 18:43:23 -0300

    Added option to create Drought Moisture Multiplier

---
+++ Hytone.Timberborn.Mods.SoilMoisture/DroughtListener.cs
@@ -0,0 +1,37 @@
+﻿using Bindito.Core;
+using Timberborn.SingletonSystem;
+using Timberborn.SoilMoistureSystem;
+using Timberborn.WeatherSystem;
+
+namespace Hytone.Timberborn.Mods.SoilMoisture
+{
+    public class DroughtListener : ILoadableSingleton
+    {
+        private EventBus _eventBus;
+        private SoilMoistureSimulationSettings _soilMoistureSimulationSettings;
+
+        [Inject]
+        public void InjectDependencies(EventBus eventBus, SoilMoistureSimulationSettings soilMoistureSimulationSettings)
+        {
+            _eventBus = eventBus;
+            _soilMoistureSimulationSettings = soilMoistureSimulationSettings;
+        }
+
+        public void Load()
+        {
+            _eventBus.Register(this);
+        }
+
+        [OnEvent]
+        public void OnDroughtStarted(DroughtStartedEvent droughtStartedEvent)
+        {
+            _soilMoistureSimulationSettings._maxMoisture = SoilMoisturePlugin.Config.DroughtMoistureDistance();
+        }
+
+        [OnEvent]
+        public void OnDroughtEnded(DroughtEndedEvent droughtEndedEvent)
+        {
+            _soilMoistureSimulationSettings._maxMoisture = SoilMoisturePlugin.Config.MoistureDistance;
+        }
+    }
+}
---
+++ Hytone.Timberborn.Mods.SoilMoisture/SoilMoistureConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using Bindito.Core;
+using TimberApi.ConfiguratorSystem;
+using TimberApi.SceneSystem;
+
+namespace Hytone.Timberborn.Mods.SoilMoisture
+{
+    [Configurator(SceneEntrypoint.InGame)]
+    public class SoilMoistureConfigurator : IConfigurator
+    {
+        public void Configure(IContainerDefinition containerDefinition)
+        {
+            containerDefinition.Bind<DroughtListener>().AsSingleton();
+        }
+    }
+}
